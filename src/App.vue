ef4c553a0d678c7dcda375816f4025b533b1f0e6
Yasunori Takahashi <hibriiiiidge@gmail.com>
2019-11-24 06:47:54 +0900

    Chapter2_1を実装

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Chapter1_1/>
     <Chapter1_2/>
     <Chapter1_3/>
+    <Chapter2_1/>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Chapter1_1 from './components/Chapter1/1.vue'
 import Chapter1_2 from './components/Chapter1/2.vue'
 import Chapter1_3 from './components/Chapter1/3.vue'
+import Chapter2_1 from './components/Chapter2/1.vue'
 
 export default {
   name: 'app',
   components: {
     Chapter1_1,
     Chapter1_2,
-    Chapter1_3
+    Chapter1_3,
+    Chapter2_1
   }
 }
 </script>
---
+++ src/components/Chapter2/1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>Chapter2_1</h3>
+    <input type="text" v-model="name" v-on:blur="hundleBlurName">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      nameValidatable: false,
+      message: ''
+    };
+  },
+  watch: {
+    name() {
+      if(this.nameValidatable) { this.nameValidate }
+    }
+  },
+  methods: {
+    nameValidate() {
+      if (this.name.length > 10) {
+        this.message = "too long";
+      } else {
+        this.message = "";
+      }
+    },
+    hundleBlurName() {
+      this.nameValidatable = true;
+      this.nameValidate();
+    }
+  }
+}
+</script>
+
+<style>
+</style>
